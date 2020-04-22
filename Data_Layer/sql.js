0aa62b23bcbdc84e72cf68e111c43dce1f165308
zhn <frontierpro.zhn@gmail.com>
2020-04-22 09:47:35 +0800

    Interaction with Mysql

---
+++ Data_Layer/sql.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql');
+
+function get_query_res(table, token, callback) {
+    console.log("--sql query--");
+    console.log(token);
+    console.log("Querying...");
+
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'zhn90507',
+        database: table
+    });
+
+    connection.connect();
+
+    connection.query(token, function(err, data) {
+        if (err) {
+            console.log(err);
+            console.log("");
+        }
+        callback(data);
+        connection.end();
+    });
+}
+
+module.exports = {
+    get_query_res
+}
