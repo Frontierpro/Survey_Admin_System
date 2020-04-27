473c7200b5610011fa237ed08e6f86ea178faabb
zhn <frontierpro.zhn@gmail.com>
2020-04-27 11:31:31 +0800

    Config Router of Web Server

--- App_Server/server.js
+++ App_Server/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var getRegisterForm = require("./get_register_form");
+var getLoginForm = require("./get_login_form");
 
 var app = express();
 
@@ -20,6 +21,16 @@ app.post("/get_register_form", function(req, res) {
     console.log("'password': " + pwd);
     console.log("");
     getRegisterForm.get_response(email, pwd, res);
+});
+
+app.post("/get_login_form", function(req, res) {
+    var email = req.body.email;
+    var pwd = req.body.pwd;
+    console.log("--post request from login.html--")
+    console.log("'email': " + email);
+    console.log("'password': " + pwd);
+    console.log("");
+    getLoginForm.get_response(email, pwd, res);
 });
 
 var server = app.listen(8080);
